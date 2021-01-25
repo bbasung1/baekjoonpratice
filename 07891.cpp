359755bf96cc98cc15bc691080eaa9ac99a4f9d1
bbasung01 <bbasung1@daum.net>
2021-01-25 20:34:55 +0900

    07891 bj pass version

--- 07891.cpp
+++ 07891.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
-
+using namespace std;
 int main(){
-    //test
+    int a;
+    cin>>a;
+    for(int i=0;i<a;i++){
+        long long a,b;
+        cin>>a>>b;
+        cout<<a+b<<endl;
+    }
 }
