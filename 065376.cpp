de83aa7c6af1a64e7d998b446a33de84e99d467f
bbasung <bbasung1@daum.net>
2021-01-27 16:42:45 +0900

    065376isn't working

---
+++ 065376.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n=0;
+    int k=1;
+    long double e=1;
+    cout<<"n"<<" "<<"e"<<endl<<"- -----------"<<endl;
+    for(n;n<10;n++){
+        if(n==0){
+            e=1;
+        }
+        else{
+            k*=n;
+            e+=(long double)1/k;
+        }
+        cout.precision(9);
+        cout<<n<<" "<<e<<endl;
+    }
+}
