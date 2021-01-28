cb42e6ad8ee91bbcdb89d67258be7cc6e2b4e8c7
bbasung <bbasung1@daum.net>
2021-01-28 11:41:05 +0900

    transfoem test

--- test.cpp
+++ test.cpp
@@ -1,11 +1,29 @@
-#include<iostream>
-#include<string>
+#include <algorithm>
+#include <iostream>
+#include <string>
 using namespace std;
-int main(){
-    string b;
-    for(int i=0;i<5;i++){
-        b.append(to_string(i));
+int main()
+{
+    while (1)
+    {
+        int count = 0;
+        char a;
+        string b;
+        getline(cin, b);
+        a = b[0];
+        if (a == '#')
+        {
+            break;
+        }
+        b.erase(0, 2);
+        transform(b.begin(), b.end(), b.begin(),::tolower);
+        for (char &c : b)
+        {
+            if (c == a)
+            {
+                count++;
+            }
+        }
+        cout << a << " " << count << endl;
     }
-    cout<<b;
-    
 }
