d0f1f5bc175e90e9834e3f311b2c5ae93a015164
bbasung01 <bbasung1@daum.net>
2021-01-30 12:59:14 +0900

    01094

--- 01094.cpp
+++ 01094.cpp
@@ -5,7 +5,14 @@ int main(){
     cin>>a;
     int b=0;
     for(int c=64;c>1;c/=2){
-        cout<<c<<endl;
+        if(a-c<0){
+            continue;
+        }
+        a-=c;
+        b++;
+    }
+    if(a==1){
+        b++;
     }
     cout<<b;
 }
