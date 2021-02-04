20f9c37db55c9b3669d85cb2d6ef7053b0d5c555
bbasung1 <bbasung1@daum.net>
2021-02-04 18:41:45 +0900

    11320

---
+++ 11320.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a;
+    cin>>a;
+    for(a;a>0;a--){
+        int b,c;
+        cin>>b>>c;
+        cout<<(b/c)*(b/c)<<endl;
+    }
+}
