f353fe3b599c9ed25284d6aac310e08258820abf
bbasung1 <bbasung1@daum.net>
2021-02-09 15:23:30 +0900

    noslove....

--- test.cpp
+++ test.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
 #include <cmath>
+#include <string>
 using namespace std;
 int main(){
     int a;
     int b[100];
     cin>>a;
-    for(a;a>0;a--){
-        cin>>b[a];
-        cout<<b[a]<<endl;
+    string c=to_string(a);
+    for(char &k:c){
+        cout<<a<<"\n";
+        int test=k-'0';
+        a+=test;
+        cout<<a<<"\n";
     }
 }
