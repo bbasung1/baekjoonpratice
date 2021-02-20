580d9f984489398dd1657d07c3353474f1ea386d
bbasung1 <bbasung1@daum.net>
2021-02-20 14:50:25 +0900

    02501

---
+++ 02501.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int a,b;
+	cin>>a>>b;
+	int c=0;
+	for(int i=1;i<=a;i++){
+		if(a%i==0){
+			c++;
+		}
+		if(c==b){
+			cout<<i<<"\n";
+			return 0;
+		}
+	}
+	cout<<"0\n";
+}
