dddfd0ff314984f0b644301b7c7c95a45caaee5f
bbasung1 <bbasung1@daum.net>
2021-02-28 13:07:37 +0900

    20953add

--- 20953.cpp
+++ 20953.cpp
@@ -5,7 +5,8 @@ int main(){
 	cin.tie(0);cout.tie(0);ios::sync_with_stdio(0);
 	int a;
 	cin>>a;
-	for(a;a>0;--a){
+	while(a--){
+        //cout<<a<<"\n";
 		int b,c;
 		cin>>b>>c;
 		cout<<(b+c)*((c+b)*(c+b-1)/2)<<"\n";
