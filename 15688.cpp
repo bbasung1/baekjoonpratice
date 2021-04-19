ca7b7b708c97d8826b23755f09593277a9285d70
bbasung <bbasung1@daum.net>
2021-04-19 11:20:46 +0900

    15688v1

--- 15688.cpp
+++ 15688.cpp
@@ -4,16 +4,15 @@
 using namespace std;
 int main(){
     cin.tie(0);cout.tie(0);ios::sync_with_stdio(0);
-    vector <int> a;
     int b;
     cin>>b;
-    while(b--){
-        int c;
-        cin>>c;
-        a.push_back(c);
+    int *a=new int[b];
+    for(int i=0;i<b;i++){
+        cin>>a[i];
     }
-    sort(a.begin(),a.end());
-    for(int e:a){
-        cout<<e<<"\n";
+    sort(a,a+b);
+    for(int i=0;i<b;i++){
+        cout<<a[i]<<"\n";
     }
+    delete[]a;
 }
