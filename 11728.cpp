64c9345c617ba34d4150fcd9600dbc83d1bca49f
bbasung <bbasung1@daum.net>
2021-04-19 11:03:41 +0900

    11728.cpp

--- 11728.cpp
+++ 11728.cpp
@@ -2,13 +2,13 @@
 #include <algorithm>
 using namespace std;
 int main(){
-    int a[2000000];
     int b,c;
 cin.tie(0);
 cout.tie(0);
 ios::sync_with_stdio(0);
     cin>>b>>c;
     int d=b+c;
+    int *a=new int[d];
     for(int i=0;i<d;i++){
         cin>>a[i];
     }
@@ -16,5 +16,6 @@ ios::sync_with_stdio(0);
     for(int i=0;i<d;i++){
         cout<<a[i]<<" ";
     }
-    cout<<endl;
+    cout<<"\n";
+    delete []a;
 }
