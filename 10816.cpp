6ce6ca5fee6806bf49a39a72fb0f1677591bc906
bbasung1 <bbasung1@daum.net>
2021-04-29 12:51:44 +0900

    10816

--- 10816.cpp
+++ 10816.cpp
@@ -1,20 +1,31 @@
+#include <unordered_map>
 #include <iostream>
 #include <algorithm>
 using namespace std;
 int main(){
     cin.tie(0);cout.tie(0);ios::sync_with_stdio(0);
+    unordered_map<int,int> m;
     int a;
     cin>>a;
-    int *b=new int[a];
-    for(int i=0;i<a;i++){
-        cin>>b[i];
+    while(a--){
+        int b;
+        cin>>b;
+        if(m.find(b)==m.end()){
+            m[b]=1;
+        }
+        else{
+            m[b]++;
+        }
     }
-    int c;
-    cin>>c;
-        for(int i=0;i<c;i++){
-            int d;
-            cin>>d;
-            cout<<count(b,b+a,d)<<" ";
+    cin>>a;
+    while(a--){
+        int b;
+        cin>>b;
+        if(m.find(b)==m.end()){
+            cout<<"0 ";
+        }
+        else{
+            cout<<m[b]<<" ";
+        }
     }
-    cout<<"\n";
 }
