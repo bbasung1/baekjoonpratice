1a85834045f13ba1cbe22850f9ee8f1a7079cae5
bbasung1 <bbasung1@daum.net>
2021-05-12 10:49:56 +0900

    01205

--- 01205.cpp
+++ 01205.cpp
@@ -1,20 +1,25 @@
 #include <iostream>
 #include <algorithm>
+#include <vector>
 using namespace std;
-int main(){
-    int n,a,p;
-    cin>>n>>a>>p;
-    int *b=new int[n+1];
-    b[0]=a;
-    for(int i=1;i<=n;i++){
-        cin>>b[i];
+int main() {
+    int n, a, p;
+    cin >> n >> a >> p;
+    vector <int>b(n + 1);
+    b[0] = a;
+    for (int i = 1; i <= n; i++) {
+        cin >> b[i];
     }
-    sort(b,b+n+1);
-    int c=find(b,b+n+1,a)-b+1;
-    if(c<=p&&c<0){
-        cout<<c<<"\n";
+    sort(b.begin(),b.end(),greater<int>());
+    int c = find(b.begin(),b.end(),a) - b.begin() + 1;
+    int d = count(b.begin(), b.end(), a);
+    if (p > n) {
+        cout << c << "\n";
     }
-    else{
-        cout<<"-1\n";
+    else if( p >= c+d-1){
+        cout << c << "\n";
+    }
+    else {
+        cout << "-1\n";
     }
 }
