fcc2d174e5de31ea283b8dcd4ca001b32ce55536
bbasung1 <bbasung1@daum.net>
2021-07-16 13:21:22 +0900

    01912v2

--- 01912.cpp
+++ 01912.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
 using namespace std;
 int main() {
     int a;
     cin >> a;
-    vector <long long>b(a + 1);
+    long long *b=new long long[a+1];
     for (int i = 1; i <= a; i++) {
         cin >> b[i];
     }
     long long ans = -1001;
+    b[0]=ans;
     for (int i = 1; i <= a; i++) {
         b[i] = max(b[i] + b[i - 1], b[i]);
         ans = max(ans, b[i]);
