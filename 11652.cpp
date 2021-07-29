d380488aa41085aa591d02d5ea1f2ad9fe51be33
bbasung1 <bbasung1@daum.net>
2021-07-29 13:56:09 +0900

    11652v2

--- 11652.cpp
+++ 11652.cpp
@@ -1,33 +1,30 @@
-#include<iostream>
-#include<algorithm>
+#include <iostream>
+#include <algorithm>
 using namespace std;
-long long card[1000000];
-int main()
-{
-	int n;
-	cin.tie(0);
-	cin.sync_with_stdio(false);
-	cin >> n;
-	for (int i = 0; i < n; i++)
-		cin >> card[i];
-	sort(card, card + n);
-	//세기위한 변수, 갯수를 비교하기위한변수
-	int cnt = 1, max = 0, index = 0;
-	for (int i = 0; i < n - 1; i++)
-	{
-		if (card[i] == card[i + 1])
-		{
-			cnt++;
-			if (cnt > max)
-			{
-				max = cnt;
-				index = i;
-			}
-		}
-		else
-			//0이아닌 1로초기화!
-			cnt = 1;
-	}
-	cout << card[index] << '\n';
-	
+//long long b[1000000];
+int main() {
+    cin.tie(0); cout.tie(0); ios::sync_with_stdio(0);
+    long long a;
+    cin >> a;
+    long long *b=new long long[a];
+    for (int i = 0; i < a; i++) {
+        cin >> b[i];
+    }
+    sort(b, b + a);
+    long long mx1 = 0;
+    long long mx2 = 1;
+    long long ans = b[0];
+    for (int i = 0; i < a - 1; i++) {
+        if (b[i] == b[i + 1]) {
+            mx2++;
+            if (mx2 > mx1) {
+                mx1 = mx2;
+                ans = b[i];
+            }
+        }
+        else {
+            mx2 = 1;
+        }
+    }
+    cout << ans << "\n";
 }
