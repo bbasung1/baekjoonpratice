eddb107fb69e2e08278bc06dfb4e55ead8867a7d
bbasung <bbasung1@daum.net>
2021-08-24 16:19:44 +0900

    12865v2

--- 12865.cpp
+++ 12865.cpp
@@ -4,13 +4,18 @@
 using namespace std;
 int main() {
     int a, b;
+    //cin.tie(0); cout.tie(0); ios::sync_with_stdio(0);
     cin >> a >> b;
-    vector <vector <int>>c(a, vector<int>(2, 0));
+    //vector <vector <int>>c(a, vector<int>(2, 0));
+    int** c=new int*[a];
     for (int i = 0; i < a; i++) {
+        c[i] = new int[2];
         cin >> c[i][0] >> c[i][1];
     }
-    vector <vector <int>>d(a + 1, vector<int>(b + 1, 0));
+    //vector <vector <int>>d(a + 1, vector<int>(b + 1, 0));
+    int** d = new int* [a + 1];
     for (int i = 0; i <= a; i++) {
+        d[i] = new int[b + 1];
         for (int j = 0; j <= b; j++) {
             if (i == 0 || j == 0) {
                 d[i][j] = 0;
