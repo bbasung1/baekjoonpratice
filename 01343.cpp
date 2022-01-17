4c65388743b32382a18184c3268a7c6b68d60c4d
bbasung1 <bbasung1@daum.net>
2022-01-17 15:58:37 +0900

    01343

--- 01343.cpp
+++ 01343.cpp
@@ -7,18 +7,28 @@ int main(){
     cin>>a;
     int cur=a.find('.');
     int before=0;
-    while(cur!=string::npos){
+    while(true){
+        cur=a.find('.',before);
+        if(cur==-1){
+            cur=a.size();
+        }
         if((cur-before)%2!=0){
             cout<<"-1\n";
             return 0;
         }
         int tmp=cur-before;
         int i=0;
-        for(;tmp%4==0;i+=4){
-            a.replace(before+i,4,"A");
+        for(;tmp>=4;i+=4){
+            a.replace(before+i,4,"AAAA");
+            tmp-=4;
         }
-        a.replace(before+i,2,"B");
-        before=cur;
+        if(tmp==2){
+        a.replace(before+i,2,"BB");
+        }
+        if(cur==a.size()){
+            break;
+        }
+        before=cur+1;
         cur=a.find(before+1,'.');
     }
     cout<<a<<"\n";
