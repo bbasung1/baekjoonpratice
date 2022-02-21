13285d4ebdbabe2b8fe8e5003bf5cffafe190ded
bbasung <bbasung1@daum.net>
2022-02-21 20:19:20 +0900

    24314v3

--- 24314.cpp
+++ 24314.cpp
@@ -1,24 +1,34 @@
 #include <iostream>
 using namespace std;
-int main(){
-    int a1,a0;
-    cin>>a1>>a0;
-    int c;
-    cin>>c;
-    int n0;
-    cin>>n0;
-    if(a1==c){
-        if(0<a0){
-            cout<<"1\n";
-        }else{
-            cout<<"0\n";
-        }
-    }else{
-        //cout<<"!!\n";
-    if(n0<-(a0/(a1-c))||a1<c){
-        cout<<"0\n";
-    }else{
-        cout<<"1\n";
-    }
-    }
+int main()
+{
+	int a1, a0;
+	cin >> a1 >> a0;
+	int c;
+	cin >> c;
+	int n0;
+	cin >> n0;
+	if (a1 == c)
+	{
+		if (0 < a0)
+		{
+			cout << "1\n";
+		}
+		else
+		{
+			cout << "0\n";
+		}
+	}
+	else
+	{
+		// cout<<"!!\n";
+		if (n0 < -(a0 / (a1 - c)) || a1 < c)
+		{
+			cout << "0\n";
+		}
+		else
+		{
+			cout << "1\n";
+		}
+	}
 }
